c063cf2e986c81af4e244ffe6c307bdb092a6267
cx11111111 <2227985643@qq.com>
2023-05-17 11:34:51 +0800

    多变量

---
+++ Multivariate Time Series Forecast/src/globals.py
@@ -0,0 +1,42 @@
+import torch
+from torch.nn import MSELoss
+from argparse import ArgumentParser
+
+# Device on which we port the model and variables
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+# Loss function: L1loss (mean absolute error)
+loss_func = MSELoss()
+
+parser = ArgumentParser()
+parser.add_argument("--mode", type=str, choices=["train", "eval"],
+                    default="train", help="Are we training or testing?")
+parser.add_argument("--pretrained_path", type=str,
+                    default="pretrained/pollution_net.pt",
+                    help="Location of the pretrained net")
+# DATASET PREPARATION ARGUMENTS
+parser.add_argument("--dataset_path", type=str, default="C:\\Users\\cx\\Desktop\\数据集\\firstturbdata.csv",
+                    help="Location of the dataset file")
+parser.add_argument("--lag", type=int, default=1,
+                    help="Time lag used for preparing train and test X-Y pairs")
+parser.add_argument("--train_ratio", type=float, default=0.6,
+                    help="Ratio for extracting the train set")
+parser.add_argument("--val_ratio", type=float, default=0.2,
+                    help="Ratio for extracting the validation set")
+# MODEL ARGUMENTS
+parser.add_argument("--hidden_dim", type=int, default=64,
+                    help="Dimension of the LSTM hidden state vector")
+parser.add_argument("--num_layers", type=int, default=3,
+                    help="Number of LSTM layers")
+# TRAINING ARGUMENTS
+parser.add_argument("--epochs", type=int, default=50,
+                    help="Number of training epochs")
+parser.add_argument("--bs", type=int, default=64,
+                    help="Batch size")
+parser.add_argument("--lr", type=float, default=2e-4,
+                    help="Learning rate")
+parser.add_argument("--wd", type=float, default=3e-10,
+                    help="L2 regularization weight decay")
+
+# Used to store training/model/dataset hyperparameters
+config = parser.parse_args()
